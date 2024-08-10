efa03da9a2958c187b43383b99d26360806eca64
amrendrayd <amrendrayd3@gmail.com>
2024-08-10 21:40:46 +0530

    Numbers and Maths

---
+++ numMath.js
@@ -0,0 +1,50 @@
+// Numbers
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const num = 23.58697;
+console.log(num.toPrecision(3));
+
+const num2 = 1000000;
+console.log(num2.toLocaleString());
+
+console.log("Maths");
+// maths
+
+console.log(Math);
+console.log(Math.abs(-10));
+console.log(Math.round(5.9));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(5.8));
+console.log(Math.max(2,1,5,7,3));
+console.log(Math.min(2,0,4,6));
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const min =10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
