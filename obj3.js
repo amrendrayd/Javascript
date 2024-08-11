105311419d9e67a1581bceddbe451b631bbc24f8
amrendrayd <amrendrayd3@gmail.com>
2024-08-11 12:23:24 +0530

    Json object in js

---
+++ obj3.js
@@ -0,0 +1,18 @@
+// destructring
+
+const course = {
+    courseName: "Javascript",
+    price: "999",
+    courseInstructor: "Amrendra"
+}
+
+const {courseInstructor} = course
+
+console.log(courseInstructor);
+
+// JSON
+
+// {
+//     "name": "Amrendra",
+//     "age": "23",
+// }
