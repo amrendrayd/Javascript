28a4fc9489cd56e86d231c746601801d022ad4f3
amrendrayd <amrendrayd3@gmail.com>
2024-08-12 23:10:39 +0530

    condition and loop in js

---
+++ cond.js
@@ -0,0 +1,19 @@
+// Nullish coalescing operator (??): null undefined
+
+
+// let val1;
+// val1 = 5 ?? 10
+
+// console.log(val1);
+
+let vall = null ?? 10
+console.log(vall);
+
+
+// ternary operator
+
+const incTeaPrice = 100
+
+incTeaPrice>=80 ? console.log("less then 80") : console.log("More then 80");
+;
+
---
+++ loop.js
@@ -0,0 +1,29 @@
+// for loop
+// control D
+
+for(let i =0; i<=10; i++){
+    console.log(i);
+}
+
+// while loop
+
+
+
+let i = 1;
+
+console.log("Multiply of 2");
+
+
+while (i<=10) {
+    console.log(2*i);
+    i++
+    
+}
+
+// do while loop
+
+do {
+    console.log(i);
+    i++
+    
+} while (i<10);
